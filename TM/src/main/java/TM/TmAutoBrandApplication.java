654bdd9c665f6644a8120917c96f2f784f052e79
MARIANTOMA\Marian <marian.toma2818@gmail.com>
2025-03-24 10:29:16 +0200

    Initial commit

---
+++ TM/src/main/java/TM/TmAutoBrandApplication.java
@@ -0,0 +1,13 @@
+package TM;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TmAutoBrandApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TmAutoBrandApplication.class, args);
+	}
+
+}
---
+++ TM/src/test/java/TM/TmAutoBrandApplicationTests.java
@@ -0,0 +1,13 @@
+package TM;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TmAutoBrandApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
